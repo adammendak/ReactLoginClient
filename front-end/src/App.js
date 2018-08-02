b2391b85b8d2ac0710fc89522ff9a3cdc839c553
adammendak <adam.mendak@gmail.com>
2018-08-02 20:49:55 +0200

    more

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,12 +16,6 @@ class App extends Component {
         };
     }
 
-    handleChangeLogin = (event) => {
-        this.setState({
-            login: event.target.value
-        });
-    };
-
     handleSubmit = (event) => {
         event.preventDefault();
         if (event.target.login.value === "" || event.target.password.value === "") {
@@ -43,9 +37,9 @@ class App extends Component {
               <div className={"container"}>
                   <div className={"jumbotron"}>
                       <LoginForm handleSubmit = {this.handleSubmit.bind(this)}
-                                 onChange={this.handleChangeLogin.bind(this)}
                       />
-                      <p className={"text-center mt-3"}>link do repo na gicie <a href={"https://github.com/adammendak/ReactLoginClient"} target={"blank"}>tutaj</a> </p>
+                      <p className={"text-center mt-3"}>link do repo frontu na gicie <a href={"https://github.com/adammendak/ReactLoginClient"} target={"blank"}>tutaj</a> </p>
+                      <p className={"text-center mt-3"}>link do repo backendu na gicie <a href={"https://github.com/adammendak/SpringBootAuthenticationServer"} target={"blank"}>tutaj</a> </p>
                       <Token authToken={this.state.isAuthenticated}
                              tokenValue={this.state.tokenValue}
                              login={this.state.login}
